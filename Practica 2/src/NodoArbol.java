4d67c56498e9c6dc0722e5058434c858d8ef029a
ZaimonLPZ <theonewhoisrich@gmail.com>
2026-05-06 19:22:20 -0500

    Update NodoArbol.java

--- Practica 2/src/NodoArbol.java
+++ Practica 2/src/NodoArbol.java
@@ -3,5 +3,17 @@ import java.util.*;
 public class NodoArbol {
     public String valor;
     public List<NodoArbol> hijos = new ArrayList<>();
-    public NodoArbol(String valor) { this.valor = valor; }
+
+    public NodoArbol(String valor) {
+        this.valor = valor;
+    }
+
+
+    public NodoArbol clonar() {
+        NodoArbol copia = new NodoArbol(this.valor);
+        for (NodoArbol hijo : this.hijos) {
+            copia.hijos.add(hijo.clonar());
+        }
+        return copia;
+    }
 }
